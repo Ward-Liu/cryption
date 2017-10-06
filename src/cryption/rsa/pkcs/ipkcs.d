fc51968b611eb2f2783d6ab9c962eb8d7512d94b
shove70 <shove@163.com>
2017-10-06 03:37:40 -0500

    Delete ipkcs.d

--- src/cryption/rsa/pkcs/ipkcs.d
+++
@@ -1,9 +0,0 @@
-module cryption.rsa.pkcs.ipkcs;
-
-import std.bigint;
-
-interface iPKCS
-{
-	static string encodeKey(BigInt n, BigInt d_e);
-	static void decodeKey(string key, out BigInt n, out BigInt d_e);
-}
